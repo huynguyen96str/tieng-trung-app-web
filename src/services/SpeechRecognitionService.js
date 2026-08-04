1a7003cfc015a3bcdf0ebfdb6d6f3e0e755f3ae0
huynguyen96str <81135553+huynguyen96str@users.noreply.github.com>
2026-08-04 11:56:02 +0700

    Make continuous mode dynamic based on device type

--- src/services/SpeechRecognitionService.js
+++ src/services/SpeechRecognitionService.js
@@ -12,7 +12,12 @@ export class SpeechRecognitionService {
 
         // Khởi tạo lại mỗi lần để tránh lỗi crash hoặc ngắt lập tức trên iOS
         this.recognition = new SpeechRecognition();
-        this.recognition.continuous = false;
+        
+        // Trên điện thoại (đặc biệt iOS) bắt buộc dùng false để tự ngắt khi nói xong
+        // Trên máy tính dùng true để người dùng tự bấm Stop
+        const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+        this.recognition.continuous = !isMobile;
+        
         // Bật lại interimResults để iOS nhận được partial transcript trước khi user bấm Stop
         this.recognition.interimResults = true;
         this.recognition.lang = 'zh-CN';
